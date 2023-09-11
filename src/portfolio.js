89841c17ee1492f301b29464a9f6d05ec515d944
GaetanFauconnier85 <fauconnier85@gmail.com>
2023-09-11 14:24:31 +0300

    init: portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,18 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://GaetanFauconnier85.github.io/portfolio',
+  title: 'GF.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Gaëtan Fauconnier',
+  role: 'Full Stack Developer ',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I have a solid foundation in several areas of expertise such as React Native, Angular, Redux, Java, etc. I am currently open to any request. I am serious, dynamic and motivated to learn and progress in contact with professionals.',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/ga%C3%ABtan-fauconnier-2542b8154/',
+    github: 'https://github.com/GaetanFauconnier85',
   },
 }
 
@@ -21,51 +20,54 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'SECOLLAB',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+        'Secollab is a project aimed at facilitating collaboration and communication among team members by providing a secure online platform for discussions, tasks, and document management.',
+    stack: ['Angular', 'Redux', 'Java', 'Neo4J'],
+    livePreview: 'https://www.sodiuswillert.com/en/products/secollab',
   },
   {
-    name: 'Project 2',
+    name: 'Trackday',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Trackday is a solution that allows you to track your performance, compare your times, and challenge yourself among speed and adrenaline enthusiasts.',
+    stack: ['React Native', 'Redux','Firebase', 'NodeJs', 'MongoDB'],
+    livePreview: 'https://trackday.cc',
   },
   {
-    name: 'Project 3',
+    name: 'StreamCard, projet personnel',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+        'StreamCard is an application that allows you to collect cards of internet personalities.',
+    stack: ['React Native', 'Firebase'],
+  },
+  {
+    name: 'En cours, projet personnel',
+    description:
+        'Development of an application designed to promote participation in cultural activities such as museums, exhibitions, and conferences through an engaging quest-based system.',
+    stack: ['React Native', 'React', 'NestJs', 'Supabase'],
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Angular',
+  'React Native',
+  'Redux',
+  'React',
+    'Firebase',
+    'Supabase',
   'HTML',
   'CSS',
   'JavaScript',
   'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'JAVA',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'fauconnier.gaetan@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
